7b72056f49fe4b31e3b20ba23f6289ced2d6d561
Daniel Wildt <dwildt@gmail.com>
2018-05-18 01:25:41 -0300

    adding sample test

--- src/com/danielwildt/BankAccountConsole.java
+++ src/com/danielwildt/BankAccountConsole.java
@@ -3,7 +3,7 @@ package com.danielwildt;
 public class BankAccountConsole {
 
 	public static void main(String[] args) {
-		System.out.println("BankAccount Travis Mode");
+		System.out.printf("BankAccount Console Mode. \n Current balance is %s", (new BankAccount(500)).getBalance());
 	}
 
 }
---
+++ test/com/danielwildt/BankAccountTest.java
@@ -0,0 +1,28 @@
+package com.danielwildt;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.danielwildt.BankAccount;
+
+public class BankAccountTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSimpleAccountBalance() {
+		BankAccount bankAccount = new BankAccount(500);
+		assertEquals(500, bankAccount.getBalance());
+	}
+
+}
