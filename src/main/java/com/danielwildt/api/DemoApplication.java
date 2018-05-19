d80bf3491fdef98ad8a0fc22ee407e5604390fce
Daniel Wildt <dwildt@gmail.com>
2018-05-19 10:09:14 -0300

    updating pom and spring boot app

--- src/main/java/com/danielwildt/api/DemoApplication.java
+++ src/main/java/com/danielwildt/api/DemoApplication.java
@@ -2,17 +2,17 @@ package com.danielwildt.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.danielwildt.model.BankAccount;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@RequestMapping("/hello")
+	String home() {
+		return "Hello World";//String.format("BankAccount Console Mode. \n Current balance is %s", (new BankAccount(500)).getBalance());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
-
-
-
-//"BankAccount Console Mode. \n Current balance is %s", (new BankAccount(500)).getBalance()
