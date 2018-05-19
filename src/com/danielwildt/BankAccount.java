a2fdc6c92ac7e91f2ad03d9577dc3238134abaa2
Daniel Wildt <dwildt@gmail.com>
2018-05-18 21:37:40 -0300

    updating account balance test Fixes #1

--- src/com/danielwildt/BankAccount.java
+++ src/com/danielwildt/BankAccount.java
@@ -2,6 +2,8 @@ package com.danielwildt;
 
 public class BankAccount {
 
+	public static final String ERROR_NEGATIVE_ACCOUNT_BALANCE = "Can't have negative account balance";
+
 	private int balance;
 	public BankAccount(int balance) {
 		this.balance = balance;
@@ -11,8 +13,13 @@ public class BankAccount {
 		return this.balance;
 	}
 
-	public void withdraw(int valueToWithdraw) {
+	//FIXME Should return exception, not string
+	public String withdraw(int valueToWithdraw) {
+		if(balance - valueToWithdraw < 0)
+			return ERROR_NEGATIVE_ACCOUNT_BALANCE;
+		
 		this.balance -= valueToWithdraw;
+		return null;
 	}
 
 }
--- test/com/danielwildt/AccountBalanceTest.java
+++ test/com/danielwildt/AccountBalanceTest.java
@@ -7,9 +7,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class AccountBalanceTest {
-
+	private BankAccount bankAccount = null; 
+			
 	@Before
 	public void setUp() throws Exception {
+		bankAccount = new BankAccount(500);
 	}
 
 	@After
@@ -18,14 +20,22 @@ public class AccountBalanceTest {
 
 	@Test
 	public void shouldBeAbleToWithdraw500Reais() {
-//		Given that I have a 500 reais balance
-		BankAccount bankAccount = new BankAccount(500);
-
-//		When I withdraw 500 reais from my account
+		//	Given that I have a 500 reais balance -- SETUP
+		//	When I withdraw 500 reais from my account
 		bankAccount.withdraw(500);
-		//		Then my balance should be 0 (zero)
+		//	Then my balance should be 0 (zero)
 		assertEquals("should be able to withdraw 500", 0, bankAccount.getBalance());
 	}
+	
+	@Test
+	public void shouldAvoidToWithdrawMoreThanAccountBalance() {
+		//Given that I have a 500 reais balance -- SETUP
+		//When I withdraw 501 reais from my account
+		String message = bankAccount.withdraw(501);
+		//Then a message shows saying "Can't have negative account balance"
+		assertEquals("should avoid withdraw", BankAccount.ERROR_NEGATIVE_ACCOUNT_BALANCE, message);
+	}
+	
 	
 	
 
